10ac38db61bad67173ab887256e26b67257e7503
Felipe Barrera <picobarrera@gmail.com>
2022-11-18 16:37:13 -0500

    En este caso aplicamos un menu desplegable movil en el lado izquierdo, tambien le aplicamos un Media Query para que a partir de ciertas distancias se quite o no se quite

--- union.js
+++ union.js
@@ -1,10 +1,19 @@
 const navbarDropdown = document.querySelector('.navbar-email');
 const menuTop = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const dropdownLeft = document.querySelector('.mobile-menu');
 
+//Añadir los escuchadores de eventos
 navbarDropdown.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleBurgerMenu);
 
+
+
+//Añadir la funciones creadas con los escuchadores de eventos.
 function toggleDesktopMenu(){
 	menuTop.classList.toggle('hide');
 }
 
-
+function toggleBurgerMenu(){
+	dropdownLeft.classList.toggle('hide');
+}
