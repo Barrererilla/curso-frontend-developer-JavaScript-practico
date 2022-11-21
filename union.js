6a92b2107e444449a2317fea008924e1a0b5c4f6
Felipe Barrera <picobarrera@gmail.com>
2022-11-21 14:09:32 -0500

    Eventos de JavaScript para aparecer una ventana con un click y volver a ocultarla con otro click, y luego si dos ventanas se abren una se cierra dependiendo de cual sa habra primero, tambien maquetar los productos de ventas con JavaScript, y hacer en un ciclo for varios de ellos con tal solo una lista con ibjetos adentro.

--- union.js
+++ union.js
@@ -1,17 +1,20 @@
 const navbarDropdown = document.querySelector('.navbar-email');
 const menuTop = document.querySelector('.desktop-menu');
+//Etiquetas traidas para hacer funcionar el menu-burger izquierdo, para aplicarle sus respectivos eventos.
 const menuBurger = document.querySelector('.menu');
 const dropdownLeft = document.querySelector('.mobile-menu');
 //Aquí hacia abajo traigo las etiquetas del carrito de compras y su funcionalidad
-const iconShopping = document.querySelector('.navbar-shopping-cart');
-const productsShopping = document.querySelector('.product-detail');
+const seeShoppingCart = document.querySelector('.navbar-shopping-cart');
+const toggleProductDetail = document.querySelector('.product-detail');
+const containCards = document.querySelector('.cards-container');
 
 
 
 //Añadir los escuchadores de eventos
 navbarDropdown.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleBurgerMenu);
-iconShopping.addEventListener('click', toggleProductsSee);
+//evento creado para hacer click en el carrito de compras.
+seeShoppingCart.addEventListener('click', toggleShoppingCart);
 
 
 
@@ -22,32 +25,94 @@ iconShopping.addEventListener('click', toggleProductsSee);
 function toggleDesktopMenu(){
 	menuTop.classList.toggle('hide');
 
-	const openMenu = menuTop.classList.contains('hide');
+	const hideView = menuTop.classList.contains('hide');
 
-	if(openMenu == false){
-		productsShopping.classList.add('hide');
+	if(hideView == false){
+		toggleProductDetail.classList.add('hide');
 	}
 }
 //Esta funcion lo que hace es con un click abrir y cerrar el menu de la hamburguesa izquierdo.
 function toggleBurgerMenu(){
 	dropdownLeft.classList.toggle('hide');
 
-	const openBurger = dropdownLeft.classList.contains('hide');
+	const hideView = dropdownLeft.classList.contains('hide');
 
-	if(openBurger == false){
-		productsShopping.classList.add('hide');
+	if(hideView == false){
+		toggleProductDetail.classList.add('hide');
 	}
 }
 //Esta funcion lo que hace es con un click abrir y cerrar el icono del carrito de compras derecho.
-function toggleProductsSee(){
-	productsShopping.classList.toggle('hide');
+function toggleShoppingCart(){
+	toggleProductDetail.classList.toggle('hide');
 
-	const openCart = productsShopping.classList.contains('hide');
+	const hideView = toggleProductDetail.classList.contains('hide');
 
-	if(openCart == false){
+	if(hideView == false){
 		dropdownLeft.classList.add('hide');
 		menuTop.classList.add('hide');
 	}
 }
 
 
+const productArray = [];
+
+productArray.push({
+	name: 'PC gammer',
+	price: 1200,
+	image: 'https://http2.mlstatic.com/D_NQ_NP_625648-MCO45642364298_042021-O.jpg',
+});
+productArray.push({
+	name: 'Teclado gammer',
+	price: 400,
+	image: 'https://dobleclicknet.com/wp-content/uploads/2021/06/quasar-blanco-2.png',
+});
+productArray.push({
+	name: 'Portatil gammer',
+	price: 1000,
+	image: 'https://corporativo.tecnoplaza.com.co/wp-content/uploads/2019/07/Portatil-Asus-ROG-Gamer-G7311--600x600.jpg',
+});
+
+
+function seeProducts(arrSee){
+	for(product of arrSee){
+		//Aquí vamos a crear el div padre de toda la información del producto.
+		const productCard = document.createElement('div');
+		productCard.classList.add('product-card');
+	
+		//vamos a crear la etiqueta de la imagen principal que representará el producto.
+		const productImage = document.createElement('img');
+		productImage.setAttribute('src', product.image);
+	
+		//Vamos a crear el contenedor div de la información del producto y del icono verde derecho inferior.
+		const productInfo = document.createElement('div');
+		productInfo.classList.add('product-info');
+	
+		//Se va a crear el nodo del div que es padre de los dos parrafos de la información del producto.
+		const divParentText = document.createElement('div');
+	
+		//Ahora se va a crear los dos parrafos de la infprmación del producto.
+		const productPrice = document.createElement('p');
+		productPrice.innerText = '$' + product.price;
+	
+		const productName = document.createElement('p');
+		productName.innerText = product.name;
+	
+		//Ahora se crea el contenedor "figure" para colocar el contenido que es la imagen.
+		const parentFigure = document.createElement('figure');
+	
+		const imageIcon = document.createElement('img');
+		imageIcon.setAttribute('src', './icons/bt_add_to_cart.svg');
+	
+		parentFigure.append(imageIcon);
+	
+		divParentText.append(productPrice, productName);
+	
+		productInfo.append(divParentText, parentFigure);
+	
+		productCard.append(productImage, productInfo);
+	
+		containCards.append(productCard);
+	}
+}
+
+seeProducts(productArray);
